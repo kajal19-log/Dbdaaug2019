5eae5160d6eaca39a85c64e4e3df698700dc8a9e
kajal19-log <54385384+kajal19-log@users.noreply.github.com>
2019-09-03 19:51:34 +0530

    do while1

---
+++ dowhile.java
@@ -0,0 +1,24 @@
+public class dowhile {  
+public static void main(String[] args) {  
+    int i=1;  
+    do{  
+        System.out.println(i);  
+    i++;  
+    }while(i<=10);  
+}  
+} 
+D:\sujal>javac dowhile.java
+
+D:\sujal>java dowhile
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+D:\sujal>
