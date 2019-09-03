54f61617b2c09275470802e7e0711000a4824fc2
kajal19-log <54385384+kajal19-log@users.noreply.github.com>
2019-09-03 19:53:20 +0530

    multiplication

---
+++ multi.java
@@ -0,0 +1,30 @@
+Q.1> 
+public class multi
+{
+   public static void main(String[] args)
+{
+int num=5,i;
+for( i=1; i<=10; i++)
+{
+ System.out.printf("%d *%d =%d\n",num,i,num*i);
+}
+}
+}
+
+
+
+D:\sujal>javac multi.java
+
+D:\sujal>java multi
+5 *1 =5
+5 *2 =10
+5 *3 =15
+5 *4 =20
+5 *5 =25
+5 *6 =30
+5 *7 =35
+5 *8 =40
+5 *9 =45
+5 *10 =50
+
+D:\sujal>
